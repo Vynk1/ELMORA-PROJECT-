3f48258314903f8b307ce99e785112429f7fc292
ELMORA Developer <99583253a@gmail.com>
2025-10-27 01:46:33 +0530

    Simplify Railway entry point

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// Railway entry point - use dynamic import for ES modules
-import('./server/server.js')
-  .then(() => {
-    console.log('Server started successfully');
-  })
-  .catch((err) => {
-    console.error('Failed to start server:', err);
-    process.exit(1);
-  });
+// Railway entry point - directly import the server
+import './server/server.js';
